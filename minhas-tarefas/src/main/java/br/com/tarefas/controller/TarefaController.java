146c28eb1d9bdb667a07d172941b7d0f27cb3aac
borba <rodrigoatjoin@gmail.com>
2021-11-12 10:24:31 -0300

    adicionado vo/dto

--- minhas-tarefas/src/main/java/br/com/tarefas/controller/TarefaController.java
+++ minhas-tarefas/src/main/java/br/com/tarefas/controller/TarefaController.java
@@ -1,10 +1,14 @@
 package br.com.tarefas.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import javax.sql.rowset.serial.SerialArray;
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.tarefas.controller.response.TarefaResponse;
 import br.com.tarefas.model.Tarefa;
 import br.com.tarefas.service.TarefaService;
 
@@ -23,23 +28,37 @@ public class TarefaController {
 	@Autowired
 	private TarefaService tarefaService;
 	
+	@Autowired
+	private ModelMapper mapper;
+	
 	@GetMapping("/tarefa")
-	public List<Tarefa> listarTarefas(@RequestParam Map<String, String> parametros) {
+	public List<TarefaResponse> listarTarefas(@RequestParam Map<String, String> parametros) {
+		List<Tarefa> tarefas = new ArrayList<>();
+		
 		if(parametros.isEmpty()) {
-			return tarefaService.listarTarefas();			
+			tarefas = tarefaService.listarTarefas();			
+		} else {
+			String descricao = parametros.get("descricao");
+			tarefas = tarefaService.consultarPorDescricao(descricao);
 		}
-		String descricao = parametros.get("descricao");
-		return tarefaService.consultarPorDescricao(descricao);
+		
+		List <TarefaResponse> response = tarefas
+				.stream()
+				.map(tarefa -> mapper.map(tarefa, TarefaResponse.class))
+				.collect(Collectors.toList());
+		return response;	
 	}
 	
 	@GetMapping("/tarefa/{id}")
-	public Tarefa consultarTarefa(@PathVariable Integer id) {
-		return tarefaService.consultarPorId(id);
+	public TarefaResponse consultarTarefa(@PathVariable Integer id) {
+		Tarefa tarefa = tarefaService.consultarPorId(id);
+		TarefaResponse response = mapper.map(tarefa, TarefaResponse.class);
+		return response;
 	}
 	
 	@PostMapping("/tarefa")
-	public Tarefa salvarTarefa(@Valid @RequestBody Tarefa tarefa) {
-		return tarefaService.salvar(tarefa);
+	public TarefaResponse salvarTarefa(@Valid @RequestBody Tarefa tarefa) {
+		return mapper.map(tarefaService.salvar(tarefa), TarefaResponse.class);
 	}
 	
 	@DeleteMapping("/tarefa/{id}")
---
+++ minhas-tarefas/src/main/java/br/com/tarefas/controller/response/TarefaResponse.java
@@ -0,0 +1,64 @@
+package br.com.tarefas.controller.response;
+
+import java.time.LocalDate;
+
+public class TarefaResponse {
+	
+	private Integer id;
+	private String descricao;
+	private String status;
+	private LocalDate dataEntrega;
+	private Integer categoriaId;
+	private Integer usuarioId;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public Integer getCategoriaId() {
+		return categoriaId;
+	}
+
+	public void setCategoriaId(Integer categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+
+	public Integer getUsuarioId() {
+		return usuarioId;
+	}
+
+	public void setUsuarioId(Integer usuarioId) {
+		this.usuarioId = usuarioId;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	
+	
+}

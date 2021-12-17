e7b1b6916684de90595adecdad0e56234c568001
borba <rodrigoatjoin@gmail.com>
2021-12-17 11:58:02 -0300

    ajuste integracao

--- minhas-tarefas/src/main/java/br/com/tarefas/security/WebSecurityConfig.java
+++ minhas-tarefas/src/main/java/br/com/tarefas/security/WebSecurityConfig.java
@@ -90,7 +90,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081","http://localhost:8080"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
